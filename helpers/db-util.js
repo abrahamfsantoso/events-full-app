efd3f262487d569186f3d8dd27b05ea2003152aa
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-08-12 20:56:43 +0700

    refactored comments fetching

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -14,10 +14,14 @@ export async function insertDocument(client, collection, document) {
   return result;
 }
 
-export async function getAllDocuments(client, collection, sort) {
+export async function getAllDocuments(client, collection, sort, filter = {}) {
   const db = client.db();
 
-  const documents = await db.collection(collection).find().sort(sort).toArray();
+  const documents = await db
+    .collection(collection)
+    .find(filter) // this changed - we use the "filter" parameter!
+    .sort(sort)
+    .toArray();
 
   return documents;
 }
